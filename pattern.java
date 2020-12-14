3a49060b1c1961f5552e0282e87c4ad35711947a
Nermain Taibah <74972181+Nermaintaibah@users.noreply.github.com>
2020-12-14 15:40:49 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Rhombus
+{
+ public static void main(string args[])
+{
+int i,j,k;
+scanner sc = new scanner (system.in);
+System.out.println("Enter the pattern number");
+int n = Sc.nextint();
+System.out.println("Enter the class");
+String S = SC.next();
+char c = charAt(0);
+for(i=1;i<-n;i++)
+{
+for(j=1;j<=(n-1);j++)
+System.out,print(" ");
+for(k=1;k<=i;k++);
+system.out,println();
+{
+for(i=n-1;i>=0;i--)
+{
+for(j=1;j<(n-i);j++)
+system.out.print(" ");
+for(k=1;k<=i;k++)
+system.out.print(c+" ");
+system.out.println();
+}
+}
+ 
